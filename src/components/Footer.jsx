137d1cd53f34a06e4613f2659b018f433f36897b
MYA Project <urusanyasif@gmail.com>
2026-05-10 19:28:06 +0700

    folder komponen

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+        <p>&copy; 2025 All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <a href="">Dimensimu 3D</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
