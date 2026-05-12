3ae050dc08555abf37891bffd7ce08ef2fc76daf
MYA Project <urusanyasif@gmail.com>
2026-05-12 16:12:00 +0700

    tmbh foto

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <h1>Tentang Kami</h1>
+      <p>DImensimu 3D adalah perusahaan yang didirikan oleh Ahmad Hafiz dan bergerak di bidang desain 3D dan cetak 3D. Kami menyediakan layanan desain 3D untuk berbagai kebutuhan, mulai dari desain produk kustom dan cetak 3D, selain itu kami juga menyediakan jasa pembuatan website profesional seperti Portofolio web, Company Profile, e-commerce untuk membantu bisnis Anda berkembang di dunia digital dan projek kustom.</p>
+    </>
+  )
+}
+
+export default page
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function Header() {
   return (
     <header className="header">
       <nav>
+        <Image src="https://cf.shopee.co.id/file/id-11134216-7rbk5-m98h1d137lzla4" alt="Logo" width={100} height={50} />
         <div className="logo">
-          <a href="">Dimensimu 3D</a>
+          <Link href="">Dimensimu 3D</Link>
+        </div>
+        <div className="nav-links">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
         </div>
       </nav>
     </header>
